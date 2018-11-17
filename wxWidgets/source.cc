8e523e190d50c7526b773f5a3394b034fb6de725
Qilong <623421878@qq.com>
2018-11-17 12:51:05 +0800

    Daily backup.

--- wxWidgets/source.cc
+++ wxWidgets/source.cc
@@ -11,12 +11,43 @@ enum
 	ID_Hello = 1
 };
 
+class MyPanel :public wxPanel {
+public:
+	MyPanel(wxFrame *parent) : wxPanel(parent) {
+		Bind(wxEVT_PAINT, &MyPanel::OnPaint, this);
+
+	};
+
+	void OnPaint(wxPaintEvent &event) {
+		wxPaintDC dc(this);
+
+		dc.DrawPoint(wxPoint(10, 10));
+		dc.DrawLine(wxPoint(15, 15), wxPoint(75, 75));
+		dc.DrawRectangle(wxPoint(80, 35), wxSize(50, 45));
+
+		const wxPoint points[] = {
+			wxPoint(120, 120),
+			wxPoint(120, 160),
+			wxPoint(140, 160),
+		};
+		dc.DrawPolygon(3, points);
+
+		dc.DrawCircle(wxPoint(280, 100), 80);
+
+		dc.SetTextForeground(wxColor(255, 0, 255));
+		dc.SetFont(wxFontInfo(12).Bold(2).FaceName(wxT("MS yahei")));
+		dc.DrawText(wxT("²âÊÔÎÄ×Ö"), wxPoint(200, 160));
+	}
+};
+
 class MyFrame : public wxFrame
 {
 public:
 	MyFrame()
 		:wxFrame(NULL, wxID_ANY, "Hello World")
 	{
+		//SetIcon(wxIcon());
+
 		wxMenu *menuFile = new wxMenu;
 		menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
 			"Help string shown in status bar for this menu item");
@@ -32,15 +63,28 @@ public:
 
 		SetMenuBar(menuBar);
 
-		CreateStatusBar();
+		CreateStatusBar(2);
 		SetStatusText("Welcome to wxWidgets!");
 
 		Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
 		Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
 		Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+
+		//MyPanel *panel = new MyPanel(this);
 	}
 
-private:
+	void OnMotion(wxMouseEvent &event)
+	{
+		if (event.Dragging())
+		{
+			wxClientDC dc(this);
+			wxPen pen(*wxRED, 1);
+			dc.SetPen(pen);
+			dc.DrawPoint(event.GetPosition());
+			dc.SetPen(wxNullPen);
+		}
+	}
+
 	void OnHello(wxCommandEvent &event)
 	{
 		wxLogMessage("Hello world from wxWidgets!");
@@ -53,16 +97,44 @@ private:
 
 	void OnAbout(wxCommandEvent &event)
 	{
-		wxMessageBox("This is a wxWidgets Hello World example",
-			"About Hello World", wxOK | wxICON_INFORMATION);
+		wxString message;
+		message.Printf(wxT("Hello and welcome to %s"), wxVERSION_STRING);
+		wxMessageBox(message, wxT("About Minimal"), wxOK | wxICON_INFORMATION, this);
+		//wxMessageBox("This is a wxWidgets Hello World example",
+			//"About Hello World", wxOK | wxICON_INFORMATION);
 	}
+
+	void OnErase(wxEraseEvent &event)
+	{
+		//wxDC *dc = event.GetDC();
+		//if (!dc) {
+		//	wxClientDC cdc(this);
+		//	dc = &cdc;
+		//}
+		//wxSize size = GetClientSize();
+
+		//wxBitmap bitmap("friends.png", wxBITMAP_TYPE_PNG);
+		//dc->Clear();
+		//dc->DrawBitmap(bitmap, wxPoint(0, 0), true);
+	}
+
+private:
+	DECLARE_EVENT_TABLE();
+
 };
+
+BEGIN_EVENT_TABLE(MyFrame, wxFrame)
+	EVT_MOTION(MyFrame::OnMotion)
+	EVT_ERASE_BACKGROUND(MyFrame::OnErase)
+END_EVENT_TABLE()
 
 class MyApp : public wxApp
 {
 public:
 	virtual bool OnInit()
 	{
+		wxImage::AddHandler(new wxPNGHandler());
+
 		MyFrame *frame = new MyFrame();
 		frame->Show(true);
 		return true;
